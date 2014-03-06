eb186d2370f80b499d442493efeca53383c662cb
Máté Marjai <mate.marjai@sumup.com>
2014-03-06 09:26:46 +0000

    Corrected the API generation.

--- lib/grape_bootstrap/commands/generate.rb
+++ lib/grape_bootstrap/commands/generate.rb
@@ -4,15 +4,13 @@ require 'active_support/core_ext'
 module GrapeBootstrap
   module Commands
     class Generate < Base
-      alias_method :model, :render_template
-      alias_method :api, :render_template
-
       def initialize path, type, options=[]
         super path, options
         if type.nil?
           throw "Type not specified"
         end
-        @type = type
+        # Ugly hack for models vs api
+        @type = 'model' == type ? 'models' : type
         @name = options.shift
 
         if @name.nil?
@@ -34,16 +32,19 @@ module GrapeBootstrap
                           "..", 
                           "templates",
                           "app",
-                          template_type.pluralize,
+                          template_type,
                           "new.rb.erb"
                         ].join('/')
         tpl = ERB.new(File.read(template_path))
-        target_path = "app/#{template_type.pluralize}/#{name.underscore}.rb"
+        target_path = "app/#{template_type}/#{name.underscore}.rb"
 
-        File.open("#{@path}/#{target_path}", 'w') do |file|
+        File.open("#{@path}/#{target_path}", 'w+') do |file|
           file << tpl.result(erb_binding.call)
         end
       end
+
+      alias_method :models, :render_template
+      alias_method :api, :render_template
     end
   end
 end
--- lib/grape_bootstrap/version.rb
+++ lib/grape_bootstrap/version.rb
@@ -1,3 +1,3 @@
 module GrapeBootstrap
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
